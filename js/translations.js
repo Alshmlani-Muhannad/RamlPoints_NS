9e75a2774301e7ffd5a35ba5fa2358d64d8f16fa
Alshmlani Muhannad <148769908+Alshmlani-Muhannad@users.noreply.github.com>
2025-09-07 19:08:15 +0000

    NOi

--- js/translations.js
+++ js/translations.js
@@ -238,6 +238,7 @@ const translations = {
         Cash_equ_MMM:"Cash equivalent transferred to your account after point transfer completion",
         Program_MMM:"Program Details",
         Conversion_BSF_MMM:"Conversion Rate: 0.004 SAR per point | Minimum: 1200 points | Processing: 3-5 hours",
+        BSF_Jana_tansfer_Nkk:"BSF Jana Transfer",
         
 
 
@@ -622,7 +623,8 @@ const translations = {
         Payment_MMM:"تحويل الدفع",
         Cash_equ_MMM:"يتم تحويل المبلغ النقدي المعادل إلى حسابك بعد إتمام عملية تحويل النقاط",
         Program_MMM:"تفاصيل البرنامج",
-        Conversion_BSF_MMM:"معدل التحويل: ٠٫٠٠٤ ريال سعودي لكل نقطة | الحد الأدنى: ١٢٠٠ نقطة | المعالجة: ٣-٥ ساعات"
+        Conversion_BSF_MMM:"معدل التحويل: ٠٫٠٠٤ ريال سعودي لكل نقطة | الحد الأدنى: ١٢٠٠ نقطة | المعالجة: ٣-٥ ساعات",
+        BSF_Jana_tansfer_Nkk:"تحويل نقاط جنى للبنك الفرنسي"
 
     }
 };
