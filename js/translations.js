22dcd6e3f9152700a79a65f3dff3e81de812ae8f
Alshmlani Muhannad <148769908+Alshmlani-Muhannad@users.noreply.github.com>
2025-09-22 15:18:57 +0000

    b

--- js/translations.js
+++ js/translations.js
@@ -355,6 +355,8 @@ const translations = {
         Processing_Time:"Processing Time",
         Per_Point:"per point",
         Rajhi_Bank_Rewards:"Al Rajhi Bank Rewards",
+        Convert_your_Al_Rajhi:"Convert your Al Rajhi Bank Rewards points into cash transfers directly to your bank account. Trusted by thousands of Saudi customers.",
+        Minimum_Points:"Minimum Points:",
         
 
         
@@ -645,6 +647,8 @@ const translations = {
         Processing_Time:"وقت المعالجة",
         Per_Point:"لكل نقطة",
         Rajhi_Bank_Rewards:"مكافآت البنك الراجحي",
+        Convert_your_Al_Rajhi:"حول نقاط مكافآت البنك الراجحي إلى تحويلات نقدية مباشرة إلى حسابك المصرفي. موثوق من قبل آلاف العملاء السعوديين.",
+        Minimum_Points:":الحد الأدنى",
         
         // Terms & Privacy
         Terms_header: "شروط الخدمة وسياسة الخصوصية - رمل بوينتس",
